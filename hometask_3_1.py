cc99ff91df9cce6a9165f01076c62e989aa4dd20
PavloKom <pakamar4@gmail.com>
2025-12-15 16:47:51 +0200

    Update hometask_3_1.py

--- hometask_3_1.py
+++ hometask_3_1.py
@@ -1,6 +1,6 @@
 first_number = float(input("Enter first number: "))
-operation = input("Enter operation: (+, -, *, /): ")
-second_number = float(input("Enter second number:"))
+operation = input("Enter operation: +, -, *, /: ")
+second_number = float(input("Enter second number: "))
 
 if operation == "+":
     print("Result:", first_number + second_number)
@@ -13,3 +13,4 @@ elif operation == "/":
         print("Result:", first_number / second_number)
     else:
         print("Error: division by zero")
+
